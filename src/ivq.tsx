69d3bfeeccb3333f3a59d9b1294185ff00ac3e96
Roy Bregman <roy.bregman@kaltura.com>
2022-01-20 17:07:19 +0200

    POC

--- src/ivq.tsx
+++ src/ivq.tsx
@@ -1,13 +1,12 @@
 import {h} from 'preact';
-import { IvqConfig } from "./types/IvqConfig";
+import {IvqConfig} from './types/IvqConfig';
+import {ReservedPresetAreas} from './enum/reservedPresetAreasEnum';
+const PRESETS = ['Playback', 'Live', 'Ads'];
 
-// @ts-ignore
 export class Ivq extends KalturaPlayer.core.BasePlugin {
   private _player: KalturaPlayerTypes.Player;
 
-  static defaultConfig: IvqConfig = {
-
-  };
+  static defaultConfig: IvqConfig = {};
 
   constructor(name: string, player: any, config: IvqConfig) {
     super(name, player, config);
@@ -18,7 +17,27 @@ export class Ivq extends KalturaPlayer.core.BasePlugin {
     return Promise.resolve();
   }
 
-  loadMedia(): void {}
+  loadMedia(): void {
+    this._player.addEventListener('firstplaying', () => {
+      setTimeout(() => {
+        this._player.pause();
+        // @ts-ignore
+        this._player.ui.addComponent({
+          label: 'kaltura-dual-screen-pip',
+          presets: PRESETS,
+          container: ReservedPresetAreas.PlayerArea,
+          get: () => (
+            // @ts-ignore
+            <KalturaPlayer.ui.components.Overlay open>
+              Welcome
+              <KalturaPlayer.ui.components.SeekBarPlaybackContainer
+                playerContainer={document.getElementsByClassName('playkit-gui-area')[0]}></KalturaPlayer.ui.components.SeekBarPlaybackContainer>
+            </KalturaPlayer.ui.components.Overlay>
+          )
+        });
+      }, 1000);
+    });
+  }
 
   static isValid(): boolean {
     return true;
