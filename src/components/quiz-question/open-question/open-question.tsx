335718c5c4d403bccfb3faa0e1c8aad11e8df5d5
Serhii <serhii.marchenko@Kaltura.com>
2022-02-14 17:33:19 +0200

    feat(FEV-1039): Reflection point and Open question (#8)
    * feat(FEV-1039): reflection point
    
    * feat(FEV-1039): open question

--- src/components/quiz-question/open-question/open-question.tsx
+++ src/components/quiz-question/open-question/open-question.tsx
@@ -1,8 +1,11 @@
 import {h} from 'preact';
+import {useCallback} from 'preact/hooks';
 import {QuestionProps, QuizTranslates} from '../../../types';
 import * as styles from './open-question.scss';
 
 const {withText, Text} = KalturaPlayer.ui.preacti18n;
+
+const MAX_LENGTH = 250;
 
 const translates = (): QuizTranslates => {
   return {
@@ -11,22 +14,21 @@ const translates = (): QuizTranslates => {
 };
 
 export const OpenQuestion = withText(translates)(({question, selected, onSelect, openQuestionPlaceHolder}: QuestionProps & QuizTranslates) => {
+  const handleChange = useCallback((e: any) => {
+    onSelect(e.target.value);
+  }, []);
   return (
     <div className={styles.openQuestionWrapper}>
       <div className={styles.questionText}>{question}</div>
-      <textarea
-        className={styles.questionAnswer}
-        value={selected}
-        placeholder={openQuestionPlaceHolder as string}
-        onChange={e => {
-          // @ts-ignore
-          onSelect(e.target.value);
-        }}
-      />
-      <div>
-        {
-          // TODO: add char counter
-        }
+      <div className={styles.textAreaWrapper}>
+        <textarea
+          className={styles.questionAnswer}
+          value={selected}
+          placeholder={openQuestionPlaceHolder as string}
+          maxLength={MAX_LENGTH}
+          onChange={handleChange}
+        />
+        <div className={styles.charCounter}>{`${selected.length}/${MAX_LENGTH}`}</div>
       </div>
     </div>
   );
