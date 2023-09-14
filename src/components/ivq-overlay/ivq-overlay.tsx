43dc65f3698e06b54ef5aca40197f64c9c7692f8
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2023-09-14 12:31:47 +0300

    feat(FEC-13313): wrap IvqOverlay with OverlayPortal

--- src/components/ivq-overlay/ivq-overlay.tsx
+++ src/components/ivq-overlay/ivq-overlay.tsx
@@ -1,5 +1,6 @@
 import {h, VNode} from 'preact';
 import * as styles from './ivq-overlay.scss';
+import {OverlayPortal} from '@playkit-js/common/dist/hoc/overlay-portal';
 
 const {Overlay} = KalturaPlayer.ui.components;
 
@@ -9,10 +10,12 @@ interface IvqOverlayProps {
 
 export const IvqOverlay = ({children}: IvqOverlayProps) => {
   return (
-    <div className={styles.ivqOverlay} aria-live="polite">
-      <Overlay open permanent>
-        {children}
-      </Overlay>
-    </div>
+    <OverlayPortal>
+      <div className={styles.ivqOverlay} aria-live="polite">
+        <Overlay open permanent>
+          {children}
+        </Overlay>
+      </div>
+    </OverlayPortal>
   );
 };
