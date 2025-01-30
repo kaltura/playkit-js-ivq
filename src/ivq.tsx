29f38b0ea6f6650cfbc9d15389e2d93d41b52472
yambenari24 <yam.benari@kaltura.com>
2025-01-30 15:51:09 +0200

    fix(SUP-45709): IVQ Editor - No Seeking forward Alert for V7 player

--- src/ivq.tsx
+++ src/ivq.tsx
@@ -1,6 +1,6 @@
 import {h} from 'preact';
 // @ts-ignore
-import {core, ui} from '@playkit-js/kaltura-player-js';
+import {core} from '@playkit-js/kaltura-player-js';
 // @ts-ignore
 import {Env} from '@playkit-js/playkit-js';
 import {FloatingItem, FloatingManager, ToastManager, ToastSeverity} from '@playkit-js/ui-managers';
