47699e21be71f6911c39520a2b253e4f3cefcf0f
valentinStoicaK <162963925+valentinStoicaK@users.noreply.github.com>
2025-03-11 15:22:26 +0200

    Fix(ADA-2332): Focus management on Quiz modals

--- src/components/ivq-overlay/ivq-overlay.tsx
+++ src/components/ivq-overlay/ivq-overlay.tsx
@@ -1,6 +1,7 @@
 import {h, VNode} from 'preact';
 import * as styles from './ivq-overlay.scss';
 import {OverlayPortal} from '@playkit-js/common/dist/hoc/overlay-portal';
+import { FocusTrap } from '@playkit-js/common/dist/components/focus-trap';
 
 const {Overlay} = KalturaPlayer.ui.components;
 
@@ -11,11 +12,13 @@ interface IvqOverlayProps {
 export const IvqOverlay = ({children}: IvqOverlayProps) => {
   return (
     <OverlayPortal>
-      <div className={styles.ivqOverlay} aria-live="polite">
-        <Overlay open permanent>
-          {children}
-        </Overlay>
-      </div>
+      <FocusTrap active>
+        <div className={styles.ivqOverlay} aria-live="polite">
+          <Overlay open permanent>
+            {children}
+          </Overlay>
+        </div>
+      </FocusTrap>
     </OverlayPortal>
   );
 };
--- src/components/quiz-submit/quiz-submit.tsx
+++ src/components/quiz-submit/quiz-submit.tsx
@@ -1,6 +1,7 @@
 import {h} from 'preact';
 import {useCallback, useState, useRef, useEffect} from 'preact/hooks';
 import {A11yWrapper} from '@playkit-js/common/dist/hoc/a11y-wrapper';
+import { FocusTrap } from '@playkit-js/common/dist/components/focus-trap';
 import {QuizTranslates} from '../../types';
 import {IvqOverlay} from '../ivq-overlay';
 import {Spinner} from '../spinner';
@@ -59,37 +60,39 @@ export const QuizSubmit = withText(translates)(({onReview, onSubmit, ...otherPro
     }
   }, []);
   return (
-    <IvqOverlay>
-      <div className={['ivq', styles.quizSubmitWrapper].join(' ')} data-testid="submitContainer">
-        <div tabIndex={0}>
-          <div className={styles.title}>{otherProps.title}</div>
-          <div className={styles.description}>{otherProps.description}</div>
-        </div>
-        <div className={styles.buttonWrapper} data-testid="submitButton">
-          {onSubmit && (
-            <A11yWrapper onClick={handleSubmitClick}>
-              <div
-                tabIndex={0}
-                className={styles.primaryButton}
-                aria-label={otherProps.submitButtonAriaLabel}
-                disabled={isLoading}
-                ref={submitButtonRef}>
-                {isLoading ? <Spinner /> : otherProps.submitButton}
-              </div>
-            </A11yWrapper>
-          )}
-          <A11yWrapper onClick={handleReviewClick}>
-            <div
-              tabIndex={0}
-              disabled={isLoading}
-              className={[onSubmit ? styles.secondaryButton : styles.primaryButton, isLoading ? styles.disabled : ''].join(' ')}
-              aria-label={otherProps.reviewButtonAriaLabel}
-              ref={reviewButtonRef}>
-              {otherProps.reviewButton}
+      <IvqOverlay>
+        <FocusTrap active>
+          <div className={['ivq', styles.quizSubmitWrapper].join(' ')} data-testid="submitContainer">
+            <div tabIndex={0}>
+              <div className={styles.title}>{otherProps.title}</div>
+              <div className={styles.description}>{otherProps.description}</div>
             </div>
-          </A11yWrapper>
-        </div>
-      </div>
-    </IvqOverlay>
+            <div className={styles.buttonWrapper} data-testid="submitButton">
+              {onSubmit && (
+                <A11yWrapper onClick={handleSubmitClick}>
+                  <div
+                    tabIndex={0}
+                    className={styles.primaryButton}
+                    aria-label={otherProps.submitButtonAriaLabel}
+                    disabled={isLoading}
+                    ref={submitButtonRef}>
+                    {isLoading ? <Spinner /> : otherProps.submitButton}
+                  </div>
+                </A11yWrapper>
+              )}
+              <A11yWrapper onClick={handleReviewClick}>
+                <div
+                  tabIndex={0}
+                  disabled={isLoading}
+                  className={[onSubmit ? styles.secondaryButton : styles.primaryButton, isLoading ? styles.disabled : ''].join(' ')}
+                  aria-label={otherProps.reviewButtonAriaLabel}
+                  ref={reviewButtonRef}>
+                  {otherProps.reviewButton}
+                </div>
+              </A11yWrapper>
+            </div>
+          </div>
+        </FocusTrap>
+      </IvqOverlay>
   );
 });
