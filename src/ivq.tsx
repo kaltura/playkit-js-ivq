ec369c45ae1d0185c18577cf342c96d83da6cea2
Valentin Stoica <162963925+valentinStoicaK@users.noreply.github.com>
2026-09-07 10:01:59 +0300

    fix(FEC-15091) - Restore seekback to its own container (#211)
    * fix(FEC-15091) - Restore seekback to its own container
    
    * FEC-15091 - Add restoreseekbar fallback

--- src/ivq.tsx
+++ src/ivq.tsx
@@ -10,6 +10,7 @@ import {
   IvqConfig,
   IvqEventTypes,
   KalturaPlayerBottomBarSelector,
+  KalturaPlayerSeekBarContainerSelector,
   KalturaPlayerSeekBarSelector,
   KalturaQuizQuestion,
   PresetAreas,
@@ -157,20 +158,26 @@ export class Ivq extends KalturaPlayer.core.BasePlugin {
     return this._player.getView().parentNode?.parentNode?.querySelector(KalturaPlayerSeekBarSelector) || null;
   };
 
+  private _getSeekBarContainerNode = () => {
+    return this._player.getView().parentNode?.parentNode?.querySelector(KalturaPlayerSeekBarContainerSelector) || null;
+  };
+
   private _updatePlayerHover = () => {
     this._player.dispatchEvent(new FakeEvent(PLAYER_HOVERED));
   };
 
   private _restoreSeekBar = () => {
-    const seekBarParentNode = this._getBottomBarNode();
-    if (seekBarParentNode && !seekBarParentNode?.querySelector(KalturaPlayerSeekBarSelector)) {
-      const seekBarNode = this._getSeekBarNode();
-      if (seekBarNode) {
-        // move player seek bar from IvqBottomBar to Kaltura player bottom bar
-        seekBarParentNode.append(seekBarNode);
-        seekBarNode.setAttribute('role', 'slider');
-      }
-    }
+    const targetNode = this._getSeekBarContainerNode() || this._getBottomBarNode();
+    if (!targetNode) return;
+    
+    if (targetNode.querySelector(KalturaPlayerSeekBarSelector)) return;
+
+    const seekBarNode = this._getSeekBarNode();
+    if (!seekBarNode) return;
+
+    // move player seek bar from IvqBottomBar to Kaltura player seekbar container
+    targetNode.append(seekBarNode);
+    seekBarNode.setAttribute('role', 'slider');
   };
 
   private _handlePlaylistConfiguration() {
--- src/types/IvqConfig.ts
+++ src/types/IvqConfig.ts
@@ -7,3 +7,5 @@ export const UiComponentArea = 'GuiArea';
 export const KalturaPlayerSeekBarSelector = '.playkit-seek-bar';
 
 export const KalturaPlayerBottomBarSelector = '.playkit-bottom-bar-area';
+
+export const KalturaPlayerSeekBarContainerSelector = '.playkit-seekbar-container';
